99522ac21542d2b23de4f1a5898420dd0b38b5a4
Jishmaab <jishma.ab@aatoon.com>
2023-10-31 15:30:33 +0530

    issue solved

--- app1/views.py
+++ app1/views.py
@@ -100,7 +100,7 @@ class LogoutView(APIView):
 class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    permission_classes = [IsTaskOwner]
+    permission_classes = [IsTaskOwner,HasAPIKey]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
